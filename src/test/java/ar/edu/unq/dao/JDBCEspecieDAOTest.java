786eb62d4843c690a534092dba2c33d65eb3d0dd
ggoffredo <gaston.goffredo@gmail.com>
2018-08-28 22:36:19 -0300

    se agrega test para el metodo recuperarTodos

--- src/test/java/ar/edu/unq/dao/JDBCEspecieDAOTest.java
+++ src/test/java/ar/edu/unq/dao/JDBCEspecieDAOTest.java
@@ -8,6 +8,9 @@ import ar.edu.unq.epers.bichomon.backend.model.especie.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class JDBCEspecieDAOTest {
 
@@ -22,7 +25,7 @@ public class JDBCEspecieDAOTest {
 		this.especie.setPeso(100);
 	    this.especie.setTipo(TipoBicho.AGUA);
 	    this.especie.setEnergiaIncial(100);
-	    this.especie.setUrlFoto("images/pablomon.jpg");
+	    this.especie.setUrlFoto("image/pablomon.jpg");
 	    this.especie.setCantidadBichos(5);
 	}
 
@@ -44,8 +47,92 @@ public class JDBCEspecieDAOTest {
 	}
 
 	@Test
-	public void al_guardar_varios_y_luego_recuperar_todos_se_obtienen_objetos_similares(){
+	public void al_guardar_varios_y_luego_recuperar_todos_se_obtienen_la_misma_Cantidad_o_mayor(){
+		List<Especie> especiesOriginales = generarTodos();
+		List<Especie> especiesRecuperadas = this.dao.recuperarTodos();
 
+		assertTrue(especiesRecuperadas.size() >= especiesOriginales.size());
 	}
 
+	/**
+	 * Crea todos los bichomongos, los guarda y lso devuelve en una lista.
+	 */
+	private List<Especie> generarTodos(){
+
+		List<Especie> especies = new ArrayList<Especie>();
+
+		Especie red = new Especie(1, "Rojomon", TipoBicho.FUEGO);
+		red.setAltura(180);
+		red.setPeso(75);
+		red.setEnergiaIncial(100);
+		red.setUrlFoto("/image/rojomon.jpg");
+		red.getNombre();
+		this.dao.guardar(red);
+		especies.add(red);
+
+		Especie amarillo = new Especie(2, "Amarillomon", TipoBicho.ELECTRICIDAD);
+		amarillo.setAltura(170);
+		amarillo.setPeso(69);
+		amarillo.setEnergiaIncial(300);
+		amarillo.setUrlFoto("/image/amarillomon.png");
+		amarillo.getNombre();
+		this.dao.guardar(amarillo);
+		especies.add(amarillo);
+
+		Especie green = new Especie(3, "Verdemon", TipoBicho.PLANTA);
+		green.setAltura(150);
+		green.setPeso(55);
+		green.setEnergiaIncial(5000);
+		green.setUrlFoto("/image/verdemon.jpg");
+		green.getNombre();
+		this.dao.guardar(green);
+		especies.add(green);
+
+		Especie turronmon = new Especie(4, "Tierramon", TipoBicho.TIERRA);
+		turronmon.setAltura(1050);
+		turronmon.setPeso(99);
+		turronmon.setEnergiaIncial(5000);
+		turronmon.setUrlFoto("/image/tierramon.jpg");
+		turronmon.getNombre();
+		this.dao.guardar(turronmon);
+		especies.add(turronmon);
+
+		Especie fantasmon = new Especie(5, "Fantasmon", TipoBicho.AIRE);
+		fantasmon.setAltura(1050);
+		fantasmon.setPeso(99);
+		fantasmon.setEnergiaIncial(5000);
+		fantasmon.setUrlFoto("/image/fantasmon.jpg");
+		fantasmon.getNombre();
+		this.dao.guardar(fantasmon);
+		especies.add(fantasmon);
+
+		Especie vampiron = new Especie(6, "Vanpiron", TipoBicho.AIRE);
+		vampiron.setAltura(1050);
+		vampiron.setPeso(99);
+		vampiron.setEnergiaIncial(5000);
+		vampiron.setUrlFoto("/image/vampiromon.jpg");
+		vampiron.getNombre();
+		this.dao.guardar(vampiron);
+		especies.add(vampiron);
+
+		Especie fortmon = new Especie(7, "Fortmon", TipoBicho.CHOCOLATE);
+		fortmon.setAltura(1050);
+		fortmon.setPeso(99);
+		fortmon.setEnergiaIncial(5000);
+		fortmon.setUrlFoto("/image/fortmon.png");
+		fortmon.getNombre();
+		this.dao.guardar(fortmon);
+		especies.add(fortmon);
+
+		Especie dientemon = new Especie(8, "Dientemon", TipoBicho.AGUA);
+		dientemon.setAltura(1050);
+		dientemon.setPeso(99);
+		dientemon.setEnergiaIncial(5000);
+		dientemon.setUrlFoto("/image/dientmon.jpg");
+		dientemon.getNombre();
+		this.dao.guardar(dientemon);
+		especies.add(dientemon);
+
+		return especies;
+	}
 }
