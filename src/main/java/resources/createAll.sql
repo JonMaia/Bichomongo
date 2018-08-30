fd59368fa32935a255fd4fa843d358a086776556
ggoffredo <gaston.goffredo@gmail.com>
2018-08-30 20:51:43 -0300

    Script de creacion corregido con PK, FK y UNIQUE

--- src/main/java/resources/createAll.sql
+++ src/main/java/resources/createAll.sql
@@ -4,20 +4,22 @@ CREATE SCHEMA bichomongo;
 USE bichomongo;
 
 CREATE TABLE bichomongo.especie (
-	id serial,
-	nombre varchar(100) NULL,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nombre NVARCHAR(100) NULL,
 	peso INT UNSIGNED NULL,
-	altura varchar(100) NULL,
-	tipo varchar(100) NULL,
+	altura VARCHAR(100) NULL,
+	tipo VARCHAR(100) NULL,
 	energia_inicial INT UNSIGNED NULL,
-	url_foto varchar(100) NULL,
-	cantidad_bichos INT NULL
-)
+	url_foto VARCHAR(100) NULL,
+	cantidad_bichos INT NULL,
+	UNIQUE (nombre)
+);
 
 CREATE TABLE bichomongo.bicho (
-    id serial,
-    nombre varchar(100),
-    especie INT NULL
-)
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(100),
+	especie INT NULL,
+	UNIQUE (nombre)
+);
 
 ALTER TABLE bichomongo.bicho ADD CONSTRAINT fk_bicho_especie FOREIGN KEY (especie) REFERENCES bichomongo.especie (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
