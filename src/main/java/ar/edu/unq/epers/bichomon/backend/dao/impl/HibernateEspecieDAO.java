1ddba98fac3ff624118f543b8c335eee398c2a99
ggoffredo <gaston.goffredo@gmail.com>
2018-09-07 17:43:42 -0300

    creado HibernateEspecieDAO que implementa EspecieDAO - falta implementar los metodos

---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/HibernateEspecieDAO.java
@@ -0,0 +1,33 @@
+package ar.edu.unq.epers.bichomon.backend.dao.impl;
+
+import ar.edu.unq.epers.bichomon.backend.dao.EspecieDAO;
+import ar.edu.unq.epers.bichomon.backend.model.especie.Especie;
+
+import java.util.List;
+
+public class HibernateEspecieDAO implements EspecieDAO {
+    @Override
+    public void guardar(Especie especie) {
+
+    }
+
+    @Override
+    public void actualizar(Especie especie) {
+
+    }
+
+    @Override
+    public Especie recuperar(String nombreEspecie) {
+        return null;
+    }
+
+    @Override
+    public List<Especie> recuperarTodos() {
+        return null;
+    }
+
+    @Override
+    public void eliminarEspecies() {
+
+    }
+}
