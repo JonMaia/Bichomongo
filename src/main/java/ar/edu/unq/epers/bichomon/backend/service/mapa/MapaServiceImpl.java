ab5e32f03677ae88baf7bb8b986165f18cb446a4
wisaku <sabaliauskaspablo@gmail.com>
2018-09-14 18:30:30 -0300

    correccion

--- src/main/java/ar/edu/unq/epers/bichomon/backend/service/mapa/MapaServiceImpl.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/service/mapa/MapaServiceImpl.java
@@ -3,21 +3,41 @@ package ar.edu.unq.epers.bichomon.backend.service.mapa;
 import ar.edu.unq.epers.bichomon.backend.model.bicho.Bicho;
 
 public class MapaServiceImpl implements MapaService {
+    @Override
+    public void mover(String entrenador, String ubicacion) {
 
+    }
+
+    @Override
+    public int cantidadEntrenadores(String ubicacion) {
+        return 0;
+    }
+
+    @Override
+    public Bicho campeon(String dojo) {
+        return null;
+    }
+
+    @Override
+    public Bicho campeonHistorico(String dojo) {
+        return null;
+    }
+/**
     void mover(String entrenador, String ubicacion) {
-    /*se cambiará al entrenador desde su ubicación actual a la especificada por parametro.*/
+    /*se cambiará al entrenador desde su ubicación actual a la especificada por parametro.
         
     }
 
     int cantidadEntrenadores(String ubicacion) {
-        /*se deberá devolver la cantidad de entrenadores que se encuentren actualmente en dicha localización.*/
+        /*se deberá devolver la cantidad de entrenadores que se encuentren actualmente en dicha localización.
     }
 
     Bicho campeon(String dojo) {
-        /*retorna el actual campeon del Dojo especificado.*/
+        /*retorna el actual campeon del Dojo especificado.
     }
 
     Bicho campeonHistorico(String dojo) {
-        /*retorna el bicho que haya sido campeon por mas tiempo en el Dojo.*/
+        /*retorna el bicho que haya sido campeon por mas tiempo en el Dojo.
     }
+    **/
 }
