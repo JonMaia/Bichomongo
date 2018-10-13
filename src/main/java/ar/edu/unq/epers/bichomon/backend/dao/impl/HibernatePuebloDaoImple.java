0d5e18256980a5e2b66c0840c58ab780babd5e40
wisaku <sabaliauskaspablo@gmail.com>
2018-10-13 17:24:37 -0300

    fix

---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/HibernatePuebloDaoImple.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.epers.bichomon.backend.dao.impl;
+
+import ar.edu.unq.epers.bichomon.backend.dao.PuebloDao;
+import ar.edu.unq.epers.bichomon.backend.model.Pueblo;
+
+public class HibernatePuebloDaoImple extends BaseHibernateDAO<Pueblo, String> implements PuebloDao{
+}
