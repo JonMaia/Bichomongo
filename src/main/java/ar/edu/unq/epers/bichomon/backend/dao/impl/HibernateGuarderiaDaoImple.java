4f0b6e9f43a961bd1104a75c0cb44931f7aa4a35
Jonathan Maia <jonathannicolas.maia@gmail.com>
2018-10-13 19:29:48 -0300

    Se crearon las implementaciones de los Dao Dojo y Guardería

---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/HibernateGuarderiaDaoImple.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.epers.bichomon.backend.dao.impl;
+
+import ar.edu.unq.epers.bichomon.backend.dao.GuarderiaDao;
+import ar.edu.unq.epers.bichomon.backend.model.Guarderia;
+
+public class HibernateGuarderiaDaoImple extends BaseHibernateDAO<Guarderia, String> implements GuarderiaDao {
+}
